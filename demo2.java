e996bc97ffc32641280953569e8e0602bff707b0
KeerthiReddy520 <keerthireddybandi.kr@gmail.com>
2026-01-31 14:51:18 +0530

    Add files via upload

---
+++ demo2.java
@@ -0,0 +1,9 @@
+class demo2
+{
+   public static void main(String[] args)
+    {
+    System.out.println("hello world");
+    System.out.println("mother therea institutions");
+    System.out.print("computer science and engineering");
+    }
+}
